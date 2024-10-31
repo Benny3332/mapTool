9fb75591fd0def33ccda1b3fc19974983677ac4d
吴泽昊 <1483322854@qq.com>
2024-10-31 14:43:04 +0800

    withdraw change

--- pythonProject/tran_tf.py
+++ pythonProject/tran_tf.py
@@ -7,13 +7,13 @@ import numpy as np
 from scipy.spatial.transform import Rotation as R
 
 # 转换矩阵，确保是numpy数组
-tr_matrix = np.array([
+tr_matrix_lidar_2_depth = np.array([
     [-0.0347702, -0.999329, -0.0115237, 0.113479],
     [0.0359214, 0.0102735, -0.999302, -0.216314],
     [0.99875, -0.0351599, 0.0355401, -0.00212184],
     [0, 0, 0, 1]
 ])
-
+tr_imu_2_lidar =np.array([11,23.29,-44.12])
 # 四元数转旋转矩阵
 def quaternion2rot(quaternion):
     r = R.from_quat(quaternion) # 顺序为 (x, y, z, w)
@@ -32,27 +32,26 @@ def read_poses(file_path):
     with open(file_path, 'r') as file:
         for line in file:
             parts = line.strip().split()
-            if len(parts) == 7:  # 假设每行7个元素，前三为平移，后四为四元数
+            if len(parts) == 7:
                 translation = np.array(parts[:3], dtype=float)
                 quaternion = np.array(parts[3:], dtype=float)
-                # 创建一个齐次坐标的位姿（这里只使用平移和四元数，不直接形成4x4矩阵）
                 pose = [translation, quaternion]
                 poses.append(pose)
     return poses
 
 
-def transform_poses(poses, tr_matrix):
+def transform_poses(poses, rotation_matrix, translation_matrix):
     """使用转换矩阵转换位姿"""
     transformed_poses = []
     for pose in poses:
         translation, quaternion = pose
         # 将四元数转换为旋转矩阵
-        rotation_matrix = quaternion2rot(quaternion)
-        tr_rotation = np.round(np.dot(rotation_matrix, tr_matrix[:3, :3] ), 8)
+        ori_rotation_matrix = quaternion2rot(quaternion)
+        tr_rotation = np.round(np.dot(ori_rotation_matrix, rotation_matrix ), 8)
         tr_quaternion = rot2quaternion(tr_rotation)
-        tr_translate = [round(translation[0] + tr_matrix[0,3],6), round(translation[1] + tr_matrix[1,3],6), round(translation[2] + tr_matrix[2,3],6)]
-        tr_tum = tr_translate + tr_quaternion
-        transformed_poses.append(tr_tum)
+        tr_translate =np.round([translation[0] + translation_matrix[0], translation[1] + translation_matrix[1], translation[2] + translation_matrix[2]],6)
+        new_pose = [tr_translate, tr_quaternion]
+        transformed_poses.append(new_pose)
     return transformed_poses
 
 
@@ -60,13 +59,15 @@ def write_poses(poses, store_path):
     """将位姿写入文件"""
     with open(store_path, 'w') as file:
         for pose in poses:
+            translation, quaternion = pose
+            combined = list(translation.flatten()) + quaternion
             # 将旋转矩阵（现在是平展的）和平移向量写回一行
-            line = ' '.join(map(str, pose))
+            line = ' '.join(map(str, combined))
             file.write(line + '\n')
 
 
 if __name__ == '__main__':
-    folder_name = 'gml_2024-10-15-19-38-50/'
+    folder_name = 'gml_2024-10-15-10-45-17/'
     file_name = 'pose_mid_360.txt'
     base_path = '/media/benny/bennyMove/data/dog_origin/'
     poses_path = base_path + folder_name + file_name
@@ -75,10 +76,18 @@ if __name__ == '__main__':
     # 读取雷达位姿
     poses = read_poses(poses_path)
 
-    # 转换位姿
-    transformed_poses = transform_poses(poses, tr_matrix)
+    # 转换位姿 mid_360_2_depth
+    transformed_poses = transform_poses(poses, tr_matrix_lidar_2_depth[:3, :3], tr_matrix_lidar_2_depth[:3, 3])
+
+    # depth_2_correct_dir
+    rot_change = R.from_euler('XYZ', [-90, 90, 0], degrees=True).as_matrix()
+    transformed_poses = transform_poses(transformed_poses, rot_change, np.zeros((3, 1)))
+    # rot_change = R.from_euler('xyz', [0, 90, 0], degrees=True).as_matrix()
+    # transformed_poses = transform_poses(transformed_poses, rot_change, np.zeros((3, 1)))
+
+    # rot_change = R.from_euler('xyz', [-90, 0, -90], degrees=True).as_matrix()
+    # transformed_poses = transform_poses(transformed_poses, rot_change, np.zeros((3, 1)))
 
     # 写入文件
     write_poses(transformed_poses, store_path)
-
     print("位姿转换完成并已写入文件。")
