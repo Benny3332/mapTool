30545acc25de0bbbbc6408ebb7d409a3b677aed1
Benny3332 <65523852+Benny3332@users.noreply.github.com>
2025-01-11 22:13:19 +0800

    Update changeRgbFileName.py

--- pythonProject/changeRgbFileName.py
+++ pythonProject/changeRgbFileName.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 import shutil
-from gmlDogRecordFilePath import file_path,file_pre_path
+from gmlDogRecordFilePath import file_path, file_pre_path
 
 def get_png_filenames(dir_path):
     file_names = []
@@ -14,24 +14,12 @@ def get_png_filenames(dir_path):
                 file_names.append(time_str)
     return file_names
 
+def process_images(file_name, base_dir, is_rgb):
+    dir_suffix = "_camera_color_image_raw" if is_rgb else "_camera_depth_image_rect_raw"
+    tran_suffix = "rgb" if is_rgb else "depth_png"
 
-def main():
-    print("This is Change RGB file function")
-    # 图片文件夹路径
-    # file_name = "gml_2024-11-05-14-49-29"
-    file_name = file_path
-    is_rgb = True
-
-    base_dir = file_pre_path
-
-    dir_path, tran_path = ('', '')
-
-    if is_rgb:
-        dir_path = os.path.join(base_dir, file_name, "_camera_color_image_raw")
-        tran_path = os.path.join(base_dir, file_name, "rgb")
-    else:
-        dir_path = os.path.join(base_dir, file_name, "_camera_depth_image_rect_raw")
-        tran_path = os.path.join(base_dir, file_name, "depth_png")
+    dir_path = os.path.join(base_dir, file_name, dir_suffix)
+    tran_path = os.path.join(base_dir, file_name, tran_suffix)
 
     if not os.path.exists(tran_path):
         os.makedirs(tran_path)
@@ -52,8 +40,15 @@ def main():
         new_file_path = os.path.join(tran_path, new_file_name)
         # 复制文件
         shutil.copy2(original_file_path, new_file_path)
-        # print(new_file_path)
 
+def main():
+    print("This is Change RGB file function")
+    # 图片文件夹路径
+    file_name = file_path
+    base_dir = file_pre_path
+
+    for is_rgb in [True, False]:
+        process_images(file_name, base_dir, is_rgb)
 
 if __name__ == "__main__":
     main()
