b3454489c19881f59b579ec95777f3ca6193dd6b
Benny3332 <1483322854@qq.com>
2025-02-20 16:43:25 +0800

    Added handling of list position

--- pythonProject/go2_nvigation_position_send.py
+++ pythonProject/go2_nvigation_position_send.py
@@ -3,13 +3,23 @@ import math
 def send_position():
     # robot = Go2_Robot()
     # [[0.0, 0.0], [5.438, 0.841]]
-    # [[5.438, 0.891], [5.438, 2.141], [5.588, 6.541], [5.588, 6.991], [5.538, 7.141], [4.288, 10.191]]
-    angle_radians = math.atan2(0.841 - 0, 5.438 - 0)
-    YAM = math.degrees(angle_radians)
-    print(YAM)
-    # robot.set_base_position(0, 0, angle_radians)
-    # robot.set_base_position(5.438, 0.841, angle_radians)
 
+    aim_list =  [[2.764, -0.015], [212.3826834323651, 165.07612046748872],
+                 [198.0, 165.0], [175.0, 178.0], [169.0, 189.0], [168.0, 190.0], [141.0, 191.0], [114.0, 191.0]]
+    start_pos = aim_list[0]
+    next_pos = [0, 0]
+    print(f"start send position")
+    print("----------------------------")
+    for i in range(len(aim_list) - 1):
+        next_pos = aim_list[i + 1]
+        print(f"next pos: {start_pos}")
+        angle_radians = math.atan2(next_pos[1] - start_pos[1], next_pos[0] - start_pos[0])
+        YAM = math.degrees(angle_radians)
+        print(f"next Yam : {YAM}")
+        print("----------------------------")
+        # robot.set_base_position(0, 0, angle_radians)
+    # robot.set_base_position(5.438, 0.841, angle_radians)
+    print(f"finish send position")
 if __name__ == '__main__':
     # node_init()
     send_position()
